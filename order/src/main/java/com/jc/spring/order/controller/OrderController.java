cca76d7db9d44b0d557b73530dfc80e99553773c
jincheng.zhang <xiaokawayixiao@qq.com>
2021-02-01 17:16:40 +0800

    333

--- order/src/main/java/com/jc/spring/order/controller/OrderController.java
+++ order/src/main/java/com/jc/spring/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.jc.spring.order.controller;
 
 import com.jc.spring.order.pojo.vo.OrderInfo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -15,16 +16,17 @@ import org.springframework.web.client.RestTemplate;
 public class OrderController {
     private RestTemplate restTemplate  = new RestTemplate();
 
-    @PostMapping
-    public OrderInfo createOrder(@RequestBody OrderInfo info, @AuthenticationPrincipal(expression = "username") String username){
-//        PriceInfo priceInfo = restTemplate.getForObject("http://localhost:8002/prices/" + info.getProductId(),PriceInfo.class);
-//        log.info("prices is {}", priceInfo != null ? priceInfo.getPrice(): "" );
-        log.info("user is {}", username);
-        return info;
-    }
+//    @PostMapping
+//    public OrderInfo createOrder(@RequestBody OrderInfo info, @AuthenticationPrincipal String username){
+////        PriceInfo priceInfo = restTemplate.getForObject("http://localhost:8002/prices/" + info.getProductId(),PriceInfo.class);
+////        log.info("prices is {}", priceInfo != null ? priceInfo.getPrice(): "" );
+//        log.info("user is {}", username);
+//        return info;
+//    }
 
     @GetMapping("{id}")
-    public OrderInfo getOrder(@PathVariable Long id, @RequestHeader(required = false) String username){
+    @PreAuthorize("#oauth2.hasScope('fly')")
+    public OrderInfo getOrder(@PathVariable Long id,@AuthenticationPrincipal String username){
         OrderInfo info = new OrderInfo();
         info.setProductId(id);
         log.info("user is {}", username);
